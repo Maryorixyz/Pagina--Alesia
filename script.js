c315c1dd7d3f9f1c18704ca96aa6b9492e3d4b6e
maryorixyz <maryoriaraujosanti@gmail.com>
2023-09-15 03:32:14 -0500

    slider agregado

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 15,
-    strech: 0,
-    depth: 300,
-    modifier: 1,
-    slideShadows: true,
-  },
-  loop: true,
-});
+// var swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 15,
+//     strech: 0,
+//     depth: 300,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   loop: true,
+// });
+
+let sections = document.querySelectorAll('section');
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 900;
+    let height = sec.offsetHeight + 800;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add('show-animate');
+    }
+    else {
+      sec.classList.remove('show-animate');
+    }
+  })
+}
