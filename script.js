640c1d775f804d1d94730a0660344c31181c41c3
maryorixyz <maryoriaraujosanti@gmail.com>
2023-09-16 14:23:17 -0500

    a ver xd xd

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
-let sections = document.querySelectorAll('section');
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 900;
-    let height = sec.offsetHeight + 800;
+// let sections = document.querySelectorAll('section');
+// window.onscroll = () => {
+//   sections.forEach(sec => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 900;
+//     let height = sec.offsetHeight + 800;
 
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('show-animate');
-    }
-    else {
-      sec.classList.remove('show-animate');
-    }
-  })
-}
+//     if (top >= offset && top < offset + height) {
+//       sec.classList.add('show-animate');
+//     }
+//     else {
+//       sec.classList.remove('show-animate');
+//     }
+//   })
+// }
 
 
 const toggler = document.querySelector(".toggler");
